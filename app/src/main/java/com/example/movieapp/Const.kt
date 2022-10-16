ab3cf3af44b37a636f29b3b50d18b01ffd5f62ad
ivansyvak <vanyafc78@gmail.com>
2022-10-16 21:18:42 +0300

    version without night mode

--- app/src/main/java/com/example/movieapp/Const.kt
+++ app/src/main/java/com/example/movieapp/Const.kt
@@ -5,3 +5,5 @@ import com.example.movieapp.data.room.repository.MoviesRepositoryRealisation
 lateinit var MAIN: MainActivity
 const val BASE_URL = "https://api.themoviedb.org/"
 lateinit var REALIZATION: MoviesRepositoryRealisation
+
+//<color name="purple_500">#172270</color>
